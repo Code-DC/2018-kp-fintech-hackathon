5b3d28c59eab21812c062fd2cceaa0d859726f70
N0rR <n0rr7882@gmail.com>
2018-04-07 02:11:19 +0900

    DB 위치 변경

--- server/config/constants.js
+++ server/config/constants.js
@@ -1,6 +1,7 @@
 export default {
     PORT: 3000,
     LOG_FORMAT: 'dev',
-    DB_URL: 'mongodb://localhost/kpfh',
+    // DB_URL: 'mongodb://localhost/kpfh',
+    DB_URL: 'mongodb://admin:Asdf!234@ds237409.mlab.com:37409/kpfh',
     JWT_SALT: '~!@#$%^&*()_+KPFH+_)(*&^%$#@!~'
 };
